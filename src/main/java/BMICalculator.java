50d2253098b234ab98b79df96065c2a2655dcae7
MartinaSallmann <s51124@edu.campus02.at>
2021-11-18 19:10:38 +0100

    BMICalculator

--- src/main/java/BMICalculator.java
+++ src/main/java/BMICalculator.java
@@ -1,12 +1,11 @@
 /*
 
-Name: Your firstname and Lastname
-Email: Your Campus02-Mail-Address
-Matrikel-Number: Your Matrikel-Number
+Name: Martina Sallmann
+Email: s51124@edu.campus02.at
+Matrikel-Number: 52100303
 
 
  */
-
 
 
 public class BMICalculator {
@@ -17,27 +16,111 @@ public class BMICalculator {
     private int bodyHeight;
     private double bodyWeight;
 
-    // TODO: Implement Constructor
+    public BMICalculator(String firstname, String lastname, char gender, int bodyHeight, double bodyWeight) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.bodyHeight = bodyHeight;
+        this.bodyWeight = bodyWeight;
 
-    // TODO: Implement getter/setter
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getBodyHeight() {
+        return bodyHeight;
+    }
+
+    public double getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public void setBodyHeight(int bodyHeight) {
+        this.bodyHeight = bodyHeight;
+    }
+
+    public void setBodyWeight(double bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
 
     private double round(double value) {
+
+        double height_new = getBodyHeight();
+        height_new = height_new/100;
+
         return Math.round(value * 100) / 100D;
     }
 
     public double calculateBMI() {
-        // TODO: Your implementation
-        return 0;
+
+        return round(bodyWeight / ((bodyHeight / 100) * (bodyHeight / 100)));
+
     }
 
     public int calculateBMICategory() {
-        // TODO: Your implementation
+
+        double bmi = calculateBMI();
+
+
+        if (gender == 'm') {
+            if (bmi < 16) {
+                return -2;
+            } else if (bmi < 18.4) {
+                return -1;
+            } else if (bmi < 24.9) {
+                return 0;
+            } else if (bmi < 34.9) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+
+        if (gender == 'w') {
+            if (bmi < 15) {
+                return -2;
+            } else if (bmi < 17.4) {
+                return -1;
+            } else if (bmi < 23.9) {
+                return 0;
+            } else if (bmi < 33.9) {
+                return 1;
+            } else {
+                return 2;
+            }
+
+        }
         return 0;
     }
 
+
     public String getBMICategoryName() {
-        // TODO: Your implementation
-        return "";
+
+       if (calculateBMICategory() == -2){
+           return "Sehr starkes Untergewicht";
+       }else if (calculateBMICategory() == -1){
+           return "Untergewicht";
+       } else if (calculateBMICategory() == 0){
+           return "Normalgewicht";
+       }else if (calculateBMICategory() == 1){
+           return "Übergewicht";
+       }else{
+           return "Sehr starkes Übergewicht";
+       }
+        
+        
     }
 
-}
+    }
+
